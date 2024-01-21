162fd2935be15b1aad37a61684a798b0072d865c
Dominic nix <dnix8715@gmail.com>
2024-01-21 14:09:34 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+  }
+  function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+  }
+  function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+  }
+  function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+  }
+  function appendCat(Broom) {
+    const newCats = [...cats, Broom];
+    return newCats;
+  }
+  function prependCat(cat) {
+    const newCats = [cat, ...cats];
+     return newCats;
+  }
+  function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
+  
